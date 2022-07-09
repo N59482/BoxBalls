49bd6da9549ce50bb0a011ce2eb5e104d6bca85b
N59482 <iduginmail@ya.ru>
2022-07-09 11:23:27 +0300

    Алгоритм

--- BoxBalls.cpp
+++ BoxBalls.cpp
@@ -1,52 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <string>
+/*
+https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
+Задача с leedcode N1769. Minimum Number of Operations to Move All Balls to Each Box
 
-using namespace std;
+You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+Each answer[i] is calculated considering the initial state of the boxes.
+Example 1:
+Input: boxes = "110"
+Output: [1,1,3]
+Explanation: The answer for each box is as follows:
+1) First box: you will have to move one ball from the second box to the first box in one operation.
+2) Second box: you will have to move one ball from the first box to the second box in one operation.
+3) Third box: you will have to move one ball from the first box to the third box in two operations, and move one ball from the second box to the third box in one operation.
+Example 2:
+Input: boxes = "001011"
+Output: [11,8,5,4,3,4]
+Constraints:
+n == boxes.length
+1 <= n <= 2000
+boxes[i] is either '0' or '1'.
+*/
 
-void show(string s)
-    {
-        for(auto & i : s)
-            {
-                cout<<i<<" ";
-            };
-        cout<<endl;
-    };
-
-void show(vector<int> v)
-    {
-        for(auto & i : v)
-            {
-                cout<<i<<" ";
-            };
-        cout<<endl;
-    };
-
+class Solution {
+public:
 vector<int> minOperations(string boxes) 
     {
-        cout<<"Foo get:\n";
-        show(boxes);
         int SIZE = boxes.length();
-        vector<int> answer(SIZE);
-        int ia = 0;
-        for(ia; ia < SIZE; ia++)
+        vector<int> answer(SIZE,0);
+        for(int ia = 0; ia < SIZE; ia++)
             {
-                for(int ib = 0; ib < (SIZE-1); ib++)
+                for(int ib = 0; ib < SIZE; ib++)
                     {
-                        while(!(boxes[ib])) ib++;        
-                        answer.at(ia) += abs(ia - ib);
-                        cout<<"ib = "<<ib<<"\nia = "<<ia<<endl;
-                        show(answer);
+                        if(boxes[ib] == '1')
+                            answer.at(ia) += abs(ia - ib);
                     };
             };
         return answer;
     };
-
-int main()
-    {
-        string boxes = "110" ;
-        vector<int> vec = minOperations(boxes);
-        cout<<"Foo returned:\n";
-        show(vec);
-        return 0;
-    }
+};
